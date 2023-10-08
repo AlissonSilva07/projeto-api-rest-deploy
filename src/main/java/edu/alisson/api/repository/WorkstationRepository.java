e4500b6a27d1e4e96e62a1919ad8e5e53144b3b9
Alisson Rego <alissondroid07@gmail.com>
2023-10-08 02:15:28 -0300

    Criado o application-dev.yml.

---
+++ src/main/java/edu/alisson/api/repository/WorkstationRepository.java
@@ -0,0 +1,9 @@
+package edu.alisson.api.repository;
+
+import edu.alisson.api.domain.model.Workstation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WorkstationRepository extends JpaRepository<Workstation, Long> {
+}
