564c66cb484a1f0e8e42f5591219d542dbff9223
Alisson Silva <alissondroid07@gmail.com>
2023-10-09 15:06:44 -0300

    Ajustes de CORS para front-end.

--- src/main/java/edu/alisson/api/controller/UserController.java
+++ src/main/java/edu/alisson/api/controller/UserController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/workstation")
+@CrossOrigin(origins = "*")
 public class UserController {
     private final WorkstationService workstationService;
 
